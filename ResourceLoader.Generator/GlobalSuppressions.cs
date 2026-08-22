e6982acbfc59b7a7227897c94777420534cee669
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-08-22 14:33:43 -0500

    supress RS1035: File IO is intentional in this generator

---
+++ ResourceLoader.Generator/GlobalSuppressions.cs
@@ -0,0 +1,16 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage(
+    "MicrosoftCodeAnalysisCorrectness",
+    "RS1035:Do not use APIs banned for analyzers",
+    Justification = "File IO is intentional in this generator",
+    Scope = "member",
+    Target = "~M:ResourceLoader.Generator.ResourceLoaderGenerator.Execute(" +
+        "Microsoft.CodeAnalysis.SourceProductionContext," +
+        "Microsoft.CodeAnalysis.CSharp.Syntax.ClassDeclarationSyntax," +
+        "System.String)")]
