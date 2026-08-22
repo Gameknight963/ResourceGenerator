3df3db6dad77d1ec5758894e279c6f77bd0481c0
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-08-22 14:23:02 -0500

    add test console application

---
+++ TestProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using ResourceLoader.Attributes;
+
+namespace TestProject
+{
+    [ResourceFolder("Resources", "_resources")]
+    internal partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
