c071ace76b69f4e4576ce189231c70770fe5de9a
Gameknight963 <88798282+Gameknight963@users.noreply.github.com>
2026-08-22 15:30:43 -0500

    update test project

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,10 +1,14 @@
 ﻿using ResourceLoader.Attributes;
+using ResourceLoader.Defaults;
 
 namespace TestProject
 {
-    [ResourceFolder("Resources", "_resources")]
+    [UseDefaultLoaders]
+    [ResourceFolder("Resources", nameof(_resources))]
     internal partial class Program
     {
+        string _resources = "shit";
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
